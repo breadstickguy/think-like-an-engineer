362c9c61db4ce1479890407324247962ebdd1c63
JJ Brenner <jbrenner@solutionstream.com>
2018-02-07 16:42:47 -0700

    initial files and thinking.js

---
+++ thinking.js
@@ -0,0 +1,41 @@
+// Principles for thinking like and engineer
+
+/* 	The goal of this thought process, is to think like an engineer, 
+*	without having to be one.
+* 	Engineers are very good at being analytical and breaking problems down into 
+*	smaller problems.
+* 	The challenge is, analytical thinking does not come naturally to everyone.
+* 	However, as programmers, we must learn how to accomplish this, even if it's hard.
+* 	Here are a few simple rules to follow through ANY exercise, 
+*	that will help you think more like an engineer.
+*/
+
+// 	Step 1:
+console.log('Always make a plan');
+/* 	Plans
+* 	Your plan should be based around the requirements of the project. 
+*	Seek to fully understand the requirements before you begin building a plan. 
+*	Always ask questions!
+*	Plans should also allow for change, should the requirements change, 
+* 	you'll need to budget for the new/changed scope.
+*/
+
+console.log('Always follow the plan');
+// 	Once the plan is set, follow it. Don't deviate unless the requirements are changing. 
+//	Stick to your plan!
+
+console.log('Always Psuedo-Code everything');
+/* 	Never start coding until you have a structure that you're going to follow!
+*	You should be able to describe, in plain english, what your component/feature does
+*	and how it works. If you cannot, then you aren't ready to start!
+*/
+
+console.log('Never break the "Single Responsibility Principle"');
+// 	Your function/class/library should be responsible for one job and one job only
+
+console.log('Keep breaking down your problem until the problem you are solving can be accomplished with one function' );
+/* 	Break it down! If you can't accomplish your goal with only one function, 
+*	then your problem is too big! Break it down into smaller problems and keep going 
+*	until you know you can solve your problem with one function.
+*/  
+
